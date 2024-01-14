eb54382da60159b012259d6183778f2cc86f3974
htregnagoCNAM <htregnago@cfai-formation.fr>
2024-01-14 09:03:47 +0100

    premier commit projet

---
+++ back/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+	use Doctrine\ORM\Tools\Setup;
+	use Doctrine\ORM\EntityManager;
+	date_default_timezone_set('America/Lima');
+	require_once "vendor/autoload.php";
+	$isDevMode = true;
+	$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+	$conn = array(
+	'host' => 'dpg-cm23hi7qd2ns73d8dut0-a.frankfurt-postgres.render.com',
+
+	'driver' => 'pdo_pgsql',
+	'user' => 'webpostgresql_user',
+	'password' => 'lhQABVBFJNU6f2C3LoYWmQmhH5a5zMKx',
+	'dbname' => 'webpostgresql',
+	'port' => '5432'
+	);
+
+
+	$entityManager = EntityManager::create($conn, $config);
+
+
+
