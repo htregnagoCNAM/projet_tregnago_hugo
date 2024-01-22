f2999d403f1ea91b9214953726112b68189d5109
htregnagoCNAM <htregnago@cfai-formation.fr>
2024-01-22 13:55:48 +0100

    Ajout composants classiques

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { PanierComponent } from './panier/panier.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'catalogue', component: CatalogueComponent },
+  { path: 'panier', component: PanierComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { PanierComponent } from './panier/panier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InscriptionComponent,
+    CatalogueComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
